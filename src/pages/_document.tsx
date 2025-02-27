ace26045d968b7381d2c17cd810f7016fc2784a7
exyzee <71382585+exyzee@users.noreply.github.com>
2025-02-27 11:16:16 +0100

    title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>ETHBelgium</title>
+        <title>ETHBelgium | Belgium's own ETH movement</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@423&display=swap"
           rel="stylesheet"
